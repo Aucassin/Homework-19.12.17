b40e5ab2f1f752961e4a5a63145efeee29950d42
Elias Boiko <elias.boiko@gmail.com>
2017-12-18 21:43:10 +0200

    Add project files.

---
+++ Homework 19.12.17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_19._12._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
